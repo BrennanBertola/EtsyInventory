fe1483d8d60be2aa5c88b0a6ef2c1ce9435c5a3e
BrennanBertola <brennan.bertola@comcast.net>
2022-04-20 12:23:54 -0600

    working on main

--- src/main.java
+++ src/main.java
@@ -19,5 +19,19 @@ public class main {
             System.out.println("error reading in api key");
             return;
         }
+
+        String options = "Please enter the number for the list of options:\n1.Add Stock\n2.Quit";
+
+        while(true) {
+            System.out.println(options);
+            int option = 2;
+
+            if (option == 1);
+                //run add stock
+            else if (option == 2)
+                break;
+        }
+
+        System.out.println("\nExiting Program");
     }
 }
